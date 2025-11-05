4107ebe9cdafc766c2a12337dfcee9cfa830ea65
Florent LE QUELLEC <florent.lequellec@hotmail.com>
2025-11-05 11:39:42 +0100

    push

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,12 @@
 #!/bin/env python
 import gm
 from pprint import pprint
-test = gm.read_delim('data/dressing.tsv')
+#test = gm.read_delim('data/dressing.tsv')
 
-pprint(test)
+#pprint(test)
 
 #test BFS sur le noeud 'chemise' dans le graphe test
-pprint(gm.BFS(test, 'sous-vetements'))
+#pprint(gm.BFS(test, 'sous-vetements'))
+
+prot_link = gm.read_delim('data/511145.protein.links.experimental.txt', column_separator=' ')
+pprint(gm.BFS(prot_link, '511145.b0014'))
